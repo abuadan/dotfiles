d09f0cf4b2473f27b1a9a47f3ae59a675c96c928
Abu Adan <abuadan@MacBook-Pro.local>
2023-01-06 14:12:09 +0000

    refactored some sh files to executables, added more macos settings

--- setup/common/terminfo.sh
+++ setup/common/terminfo.sh
@@ -7,10 +7,12 @@ set +e
 ############ better echo and path to config ############
 ############ files repo                     ############
 
-DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P || exit )"
+SCRIPT=$(realpath -s "$0")
+DIR=$(dirname "$SCRIPT")
+# DIR="$( cd -- "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
 
-. "$DIR/utils/pretty_echo.sh" --source-only
-. "$DIR/utils/dotfiles_home.sh" --source-only
+. "$DIR/../utils/pretty_echo.sh" --source-only
+. "$DIR/../utils/dotfiles_home.sh" --source-only
 
 title "Configuring terminfo"
 

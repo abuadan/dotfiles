a5a67f32c7d47fcdfbbc0f7ab38fe7a2e56bd07e
Abu Adan <abubakar.adan@kinandcarta.com>
2024-05-20 15:06:20 +0100

    Fix setup issues and update tooling

--- install.sh
+++ install.sh
@@ -2,16 +2,18 @@
 
 set +ex
 
-SCRIPT=$(realpath -s "$0")
+SCRIPT=$(realpath "$0")
 DIR=$(dirname "$SCRIPT")
 
 . "$DIR/setup/utils/pretty_echo.sh" --source-only
 . "$DIR/setup/utils/dotfiles_home.sh" --source-only
 
+info "Need to update recurive git submodules"
+git submodule update --init --remote --recursive
+
 common_setup() {
 
-	for entry in "$DOTFILES/setup/common"/*
-	do
+	for entry in "$DOTFILES/setup/common"/*; do
 		if test -f "$entry"; then
 			info "found $entry"
 		fi
@@ -22,10 +24,39 @@ usage() {
 	info ""
 }
 
+darwin_setup() {
+
+	info "Setup dot files linked to home directory"
+	chmod +x config/.config/zsh/setup.sh
+	config/.config/zsh/setup.sh
+
+	info "Link all folders/files assoisated with .config folder"
+	ln -sf "$DIR"/config/.config "$HOME"/.config
+
+	warning "Installing OH my zsh"
+	chmod +x config/.config/shell/zsh/setup/001-install-ohmyzsh
+	chmod +x config/.config/shell/zsh/setup/005-p10k.sh
+
+	config/.config/shell/zsh/setup/001-install-ohmyzsh
+	config/.config/shell/zsh/setup/005-p10k.sh
+
+	info "setting up darwin settings"
+	chmod +x setup/macos/brew
+	chmod +x setup/macos/settings
+
+	setup/macos/settings
+	setup/macos/brew
+
+	info "Update terminal settings"
+	chmod +x setup/terminal/terminfo.sh
+	setup/terminal/terminfo.sh
+
+}
 
 common_setup
-# if [[ "$(uname)" == "Darwin" ]]; then
-# 	./setup/macos/install.sh
-# fi
+if [[ "$(uname)" == "Darwin" ]]; then
+	darwin_setup
+fi
+
 echo -e
 success "Done."
